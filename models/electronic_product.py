f4f21d23958a0d78a1c222a650cb1df3fa91ea54
benaya josua <benaya.josua@gmail.com>
2025-12-01 20:23:35 +0700

    penjelasan electronic product

--- models/electronic_product.py
+++ models/electronic_product.py
@@ -1,9 +1,15 @@
 from product import Product
 
 class ElectronicProduct(Product):
+    # inisiasi constructor product dengan 5 parameter : 
+    # id, name, price, category, brand
     def __init__(self, id, name, price, category, brand, warranty_months):
+        # panggil constructor Product (parent)
         super().__init__(id, name, price, category, brand)
+        # atribut tambahan > garansi bulanan
         self.warranty_months = warranty_months
 
+    # method untuk menampilkan info produk elektronik
+    # method yang di override dari class parent (Product)
     def get_info(self):
         return f"[Elektronik] {self.name} | Brand: {self.brand} | Garansi: {self.warranty_months} Bulan"
