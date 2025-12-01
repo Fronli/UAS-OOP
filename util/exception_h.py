7423e01241d14c6525516cfdefb052f38f3d405b
benaya josua <benaya.josua@gmail.com>
2025-12-01 20:56:17 +0700

    penjelasan untuk util

--- util/exception_h.py
+++ util/exception_h.py
@@ -1,19 +1,24 @@
+# exception khusus ketika stok barang tidak cukup saat transaksi/restock/checkout
 class InsufficientStockErrorH(Exception):
     """Stock tidak mencukupi!"""
     pass
 
+# exception untuk mencegah harga negatif pada produk
 class NegativePrice(Exception):
     """Ngasih harga gk boleh negatif!"""
     pass
 
+# exception saat penambahan produk ke database gagal (misal: SQL error)
 class FailedAddingProduct(Exception):
     """Gagal nambahin product!"""
     pass
 
+# exception saat perubahan harga produk gagal (misal: ID tidak ditemukan)
 class FailedChangingPrice(Exception):
     """Gagal merubah harga product!"""
     pass
 
+# exception jika gagal menambah quantity stok (misal: DB error)
 class FailedAddingQuantity(Exception):
     """Gagal menambahkan quantity product!"""
     pass
