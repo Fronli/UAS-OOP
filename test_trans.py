7078b4a191b5091bf95939129ff3c501024f70c0
benaya josua <benaya.josua@gmail.com>
2025-12-01 21:05:07 +0700

    tambahin testingnya

--- test_trans.py
+++ test_trans.py
@@ -1,5 +1,6 @@
 import unittest
 import sys
+# tambahkan folder models/ agar bisa import model class
 sys.path.append('models/')
 
 from models.electronic_product import ElectronicProduct
@@ -8,16 +9,16 @@ from models.transaction_item import TransactionItem
 class TestTransactionLogic(unittest.TestCase):
     
     def setUp(self):
-        # Dummy data
+        # dummy data
         self.laptop = ElectronicProduct(11, "Laptop Gaming", 100, "Laptop", "Asus", 24)
     
     def test_product_encapsulation(self):
-        # Buat ngetest kalo ngasih harg anegatif
+        # buat ngetest kalo ngasih harg anegatif
         with self.assertRaises(ValueError):
             self.laptop.price = -5000
 
     def test_transaction_item_subtotal(self):
-        # Buat ngetest fitur hitung harga
+        # buat ngetest fitur hitung harga
         qty = 2
         item = TransactionItem(self.laptop, qty)
         expected_total = 200
